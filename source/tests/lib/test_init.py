9d30857e981da7742c61f744954868d7f69b0032
alekseyl1992 <alekseyl@list.ru>
2014-10-03 16:22:20 +0400

    91%, f*ck yeah!

--- source/tests/lib/test_init.py
+++ source/tests/lib/test_init.py
@@ -92,3 +92,99 @@ class InitTestCase(unittest.TestCase):
         url = lib.fix_market_url(test_url)
 
         self.assertNotEqual(url, web_url + test_app)
+
+    def test_check_for_meta(self):
+        content = r"""
+            <html>
+                <head>
+                    <meta http-equiv="refresh" content="5; url=page.html/">
+                </head>
+                <body>
+                </body>
+            </html>"""
+
+        url_prefix = 'http://mail.ru'
+
+        url = lib.check_for_meta(content, url_prefix)
+
+        assert url, 'No meta tag found'
+        assert url.startswith(url_prefix), 'supplied url prefix not took in account'
+
+    def test_get_url_http(self):
+        url = 'http://mail.ru'
+        timeout = 10
+        expected_redirect_url = 'page.html'
+        expected_content = r"""
+            <html>
+                <head>
+                    <meta http-equiv="refresh" content="5; url=page.html">
+                </head>
+                <body>
+                </body>
+            </html>"""
+
+        with mock.patch('source.lib.make_pycurl_request', mock.Mock(return_value=(expected_content, expected_redirect_url))):
+            redirect_url, redirect_type, content = lib.get_url(url, timeout)
+
+            self.assertEquals(redirect_url, expected_redirect_url, 'redirect_url not match')
+            self.assertEquals(content, expected_content, 'content not match')
+
+    def test_get_url_html(self):
+        url = 'http://mail.ru'
+        timeout = 10
+        expected_redirect_url = 'http://mail.ru/page.html'
+        expected_content = r"""
+            <html>
+                <head>
+                    <meta http-equiv="refresh" content="5; url=page.html">
+                </head>
+                <body>
+                </body>
+            </html>"""
+
+        with mock.patch('source.lib.make_pycurl_request', mock.Mock(return_value=(expected_content, None))):
+            redirect_url, redirect_type, content = lib.get_url(url, timeout)
+
+            self.assertEquals(redirect_url, expected_redirect_url, 'redirect_url not match')
+            self.assertEquals(content, expected_content, 'content not match')
+
+    def test_get_redirect_history(self):
+        expected_history_types = ['meta_tag', 'meta_tag']
+        expected_history_urls = ['http://mail.ru/', 'http://mail.ru/a.html', 'http://mail.ru/b.html']
+        expected_counters = []
+
+        content1 = r"""
+            <html>
+                <head>
+                    <meta http-equiv="refresh" content="5; url=a.html">
+                </head>
+                <body>
+                </body>
+            </html>"""
+
+        content2 = r"""
+            <html>
+                <head>
+                    <meta http-equiv="refresh" content="5; url=b.html">
+                </head>
+                <body>
+                </body>
+            </html>"""
+
+        content3 = r"""
+            <html>
+                <head>
+                </head>
+                <body>
+                </body>
+            </html>"""
+
+        with mock.patch('source.lib.get_url', mock.Mock(side_effect=[
+            (expected_history_urls[1], expected_history_types[0], content1),
+            (expected_history_urls[2], expected_history_types[1], content2),
+            (None, None, content3)
+        ])):
+            history_types, history_urls, counters = lib.get_redirect_history(expected_history_urls[0], timeout=10)
+            self.assertEquals(history_urls, expected_history_urls, 'history_urls not match')
+            self.assertEquals(history_types, expected_history_types, 'history_types not match')
+            self.assertEquals(counters, expected_counters, 'counters not match')
